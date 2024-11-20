cccdec52cf909d0e4db0d45fd57c431ed006c844
michaelfast1234 <michael.f@live.dk>
2024-11-20 15:10:29 +0100

    trying mqtt

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import paho.mqtt.client as mqtt
 import time
+from receive import LoraReceiver
 
 # Define the MQTT broker details
 BROKER = "test.mosquitto.org"  # You can replace this with your broker address
 PORT = 1883  # Default port for non-TLS connections
-TOPIC = "test/olejensen/topic"  # The MQTT topic you want to publish to
+TOPIC = "Co2/123"  # The MQTT topic you want to publish to
 
 # Create an MQTT client object
 client = mqtt.Client()
@@ -25,15 +26,25 @@ client.connect(BROKER, PORT, 60)
 # Start the loop to maintain connection with the broker
 client.loop_start()
 
+lora = LoraReceiver()
 # Loop to send messages every 20 seconds
 try:
     while True:
-        message = "Hello, MQTT!"
-        client.publish(TOPIC, message)
-        print(f"Message sent: {message}")
-        time.sleep(20)  # Wait for 20 seconds before sending the next message
+        #packet, rssi = lora.read_packet()
+        packet = [49, 59, 57, 56, 54, 59, 50, 53, 59, 51, 50]
+        if packet:
+            decoded_packet = ''.join([chr(byte) for byte in packet])
+            parts = decoded_packet.split(";")
+            message = [{"name": str(parts[0]),
+                        "temperature": int(parts[2]), 
+                        "co2" : int(parts[1]), 
+                        "humidity": int(parts[3]) }]
+            client.publish(TOPIC, message)
+            print(f"Message sent: {message}")
+            time.sleep(20)  # Wait for 20 seconds before sending the next message
 except KeyboardInterrupt:
     print("Stopping...")
+    # lora.close()
 
 # Stop the loop and disconnect from the broker
 client.loop_stop()
--- receive.py
+++ receive.py
@@ -2,75 +2,84 @@ import spidev
 import RPi.GPIO as GPIO
 import time
 
-# Initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(25, GPIO.OUT)  # Change 25 to your SX1278 NSS pin
 
-# Initialize SPI
-spi = spidev.SpiDev()
-spi.open(0, 0)  # Open SPI bus 0, device 0
-spi.max_speed_hz = 5000000  # Set SPI speed to 5 MHz
+class LoraReceiver:
+    def __init__(self):
+        # Initialize GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(25, GPIO.OUT)  # Change 25 to your SX1278 NSS pin
 
-# SX1278 Register Configuration
-def write_register(register, value):
-    GPIO.output(25, GPIO.LOW)  # Select the SX1278
-    spi.xfer2([register | 0x80, value])  # Write to register
-    GPIO.output(25, GPIO.HIGH)  # Deselect the SX1278
+        # Initialize SPI
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0)  # Open SPI bus 0, device 0
+        self.spi.max_speed_hz = 5000000  # Set SPI speed to 5 MHz
+        
+        
+    # SX1278 Register Configuration
+    def write_register(self, register, value):
+        GPIO.output(25, GPIO.LOW)  # Select the SX1278
+        self.spi.xfer2([register | 0x80, value])  # Write to register
+        GPIO.output(25, GPIO.HIGH)  # Deselect the SX1278
 
-def read_register(register):
-    GPIO.output(25, GPIO.LOW)  # Select the SX1278
-    response = spi.xfer2([register & 0x7F, 0x00])  # Read from register
-    GPIO.output(25, GPIO.HIGH)  # Deselect the SX1278
-    return response[1]
+    def read_register(selc, register):
+        GPIO.output(25, GPIO.LOW)  # Select the SX1278
+        response = self.spi.xfer2([register & 0x7F, 0x00])  # Read from register
+        GPIO.output(25, GPIO.HIGH)  # Deselect the SX1278
+        return response[1]
 
-# SX1278 Initialization
-def sx1278_init():
-    write_register(0x01, 0x80 | 0x00)  # Set to sleep mode to configure
-    write_register(0x06, 0x6C)  # Set frequency to 433 MHz
-    write_register(0x07, 0x80)
-    write_register(0x08, 0x00)
+    # SX1278 Initialization
+    def sx1278_init(self):
+        self.write_register(0x01, 0x80 | 0x00)  # Set to sleep mode to configure
+        self.write_register(0x06, 0x6C)  # Set frequency to 433 MHz
+        self.write_register(0x07, 0x80)
+        self.write_register(0x08, 0x00)
 
-    # Set base addresses
-    write_register(0x0E, 0x00)  # FIFO Tx base address
-    write_register(0x0F, 0x00)  # FIFO Rx base address
+        # Set base addresses
+        self.write_register(0x0E, 0x00)  # FIFO Tx base address
+        self.write_register(0x0F, 0x00)  # FIFO Rx base address
 
-    # Set LNA boost
-    write_register(0x0C, read_register(0x0C) | 0x03)
+        # Set LNA boost
+        self.write_register(0x0C, read_register(0x0C) | 0x03)
 
-    # Set auto AGC
-    write_register(0x26, 0x04)
+        # Set auto AGC
+        self.write_register(0x26, 0x04)
 
-    write_register(0x4D, 0x84)  # Set PA ramp-up time
-    write_register(0x09, 0x80 | 0x0F)  # Maximum output power
+        self.write_register(0x4D, 0x84)  # Set PA ramp-up time
+        self.write_register(0x09, 0x80 | 0x0F)  # Maximum output power
 
-    write_register(0x01, 0x80 | 0x05)  # Set to continuous receive mode
+        self.write_register(0x01, 0x80 | 0x05)  # Set to continuous receive mode
 
-def read_packet():
-    if read_register(0x12) & 0x40:  # Check if there's a received packet
-        write_register(0x0D, read_register(0x10))  # Set FIFO address to read
+    def read_packet(self):
+        if self.read_register(0x12) & 0x40:  # Check if there's a received packet
+            self.write_register(0x0D, self.read_register(0x10))  # Set FIFO address to read
 
-        packet = []
-        packet_length = read_register(0x13)  # Length of the packet
-        for _ in range(packet_length):
-             packet.append(read_register(0x00))  # Read from FIFO
-        #packet = read_register(0x00)
-        rssi = read_register(0x1A) - 157  # Calculate RSSI
+            packet = []
+            packet_length = self.read_register(0x13)  # Length of the packet
+            for _ in range(packet_length):
+                packet.append(self.read_register(0x00))  # Read from FIFO
 
-        write_register(0x0D, 0x00)  # Set FIFO pointer to the start
-        write_register(0x12, 0x40)  # Clear the RX done flag
-        write_register(0x01, 0x80 | 0x05)  # Restart continuous receive mode
-        return packet, rssi
-    return None, None
+            rssi = self.read_register(0x1A) - 157  # Calculate RSSI
+
+            self.write_register(0x0D, 0x00)  # Set FIFO pointer to the start
+            self.write_register(0x12, 0x40)  # Clear the RX done flag
+            self.write_register(0x01, 0x80 | 0x05)  # Restart continuous receive mode
+            return packet, rssi
+        return None, None
+    def close(self):
+        self.spi.close()
+        GPIO.cleanup()
 
 # Main Function
+lora = LoraReceiver()
+
 def main():
-    sx1278_init()
+    lora.sx1278_init()
     print("LoRa Receiver started")
     val = 0
     in_string = ""
 
     while True:
-        packet, rssi = read_packet()
+        packet, rssi = lora.read_packet()
         if packet:
             val = packet
             print(f"Received: {val}, RSSI: {rssi}")
@@ -81,5 +90,4 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        GPIO.cleanup()
-        spi.close()
+        lora.close()
