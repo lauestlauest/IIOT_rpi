c305ac96c391eee2e1a78f5b20d9d3189f310a95
michaelfast1234 <michael.f@live.dk>
2024-11-27 09:42:29 +0100

    added motordriver

---
+++ actuatorDriver.py
@@ -0,0 +1,25 @@
+import time
+import RPi.GPIO as GPIO
+
+class ActuateDriver:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(20, GPIO.OUT)
+        GPIO.setup(21, GPIO.OUT)
+
+    def raiseFlag(self):
+        GPIO.output(20, GPIO.HIGH)
+        GPIO.output(21, GPIO.LOW)
+    
+    def lowerFlag(self):
+        GPIO.output(20, GPIO.LOW)
+        GPIO.output(21, GPIO.HIGH)
+
+
+actuator = ActuateDriver()
+
+def main():
+    actuator.raiseFlag()
+    time.sleep(5)
+    actuator.lowerFlag()
+    time.sleep(5)
